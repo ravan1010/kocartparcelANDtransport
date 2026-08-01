00dea0a3cf179caec0d966bf8b0089292ff83b95
ravan1010 <ravanravana177@gmail.com>
2026-08-01 20:30:08 +0530

    main

--- src/pages/Log.jsx
+++ src/pages/Log.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Login() {
   console.log('')
   const handleGoogleLogin = () => {
-    window.open("https://serverside.kocart.online/auth/google/parcelANDtransport", "_self");
+    window.open("https://serverside.kocart.online/auth/google/parcelandtransport", "_self");
   };
 
   return (
