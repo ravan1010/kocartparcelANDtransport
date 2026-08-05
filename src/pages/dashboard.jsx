345d1d553c38502c8a83f1af2e93258b68c7ac2a
ravan1010 <ravanravana177@gmail.com>
2026-08-06 01:22:24 +0530

    mai

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -118,7 +118,7 @@ const Dashboard = () => {
 
           </div>
 
-          {serviceType === bike_parcel && (
+          {serviceType === "bike_parcel" && (
             <>
               {/* Center - Dashboard Cards */}
               <div className="grid grid-cols-2 md:grid-cols-4 gap-4 flex-1">
