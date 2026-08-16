a18e429500043f04dcabba91e9fd3a4955096601
ravan1010 <ravanravana177@gmail.com>
2026-08-16 12:41:42 +0530

    main

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -150,9 +150,9 @@ onChange={(e) => setPhoneNumber(e.target.value)}
                 onChange={(e) => setserviceType(e.target.value)}
                 className="w-full px-3 py-0 border-1 outline-none overflow-scroll mb-4 h-10"
               >                
-                <option value="goods_auto"> select </option>  
+                <option value=""> select </option>  
                 <option value="goods_auto"> 3 wheel goods auto </option>
-                {/* <option value="auto_passenger">auto passenger</option> */}
+                <option value="4_wheel_goods_auto">4 wheel goods auto</option>
                 {/* <option value="FashionANDApparel">Fashion & Apparel</option>
                 <option value="ElectronicsANDGadgets">Electronics & Gadgets</option>
                 <option value="BeautyANDPersonalCare">Beauty & Personal Care</option>
